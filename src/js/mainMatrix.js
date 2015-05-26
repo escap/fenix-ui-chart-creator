a2593b4a34473f5fe212476773414b24c6aacac3
simonefenix <simone.murzilli@fao.org>
2015-05-26 12:13:42 +0200

    test charts

--- src/js/mainMatrix.js
+++ src/js/mainMatrix.js
@@ -23,20 +23,86 @@ requirejs(['./paths'], function (paths) {
                 onReady: renderCharts
             });
 
-
             function renderCharts(creator) {
-                var chartOne, chartTwo;
 
                 var chartOne = creator.render({
                     container: "#chartOne"
                 });
-
-                var chartTwo = creator.render({
-                    container: "#chartTwo"
-                });
-
             };
+        });
 
-        })
+
+        $.getJSON("tests/resources/matrix/no_date.json", function (model) {
+
+            var creator = new ChartCreator();
+
+            creator.init({
+                model: model,
+                adapter: {
+
+                },
+                template: {},
+                creator: {},
+                onReady: renderCharts
+            });
+
+
+            function renderCharts(creator) {
+
+                var chartOne = creator.render({
+                    container: "#chartTwo",
+                    creator: {
+                        chartObj: {
+                            chart: {
+                                type: "column"
+                            }
+                        }
+                    },
+                });
+            };
+        });
+
+
+        $.getJSON("tests/resources/matrix/rankings.json", function (model) {
+
+
+            // reshape model data (rankings has it's own join data method)
+            var data = [];
+            model.forEach(function(row) {
+                data.push([row[0],row[1], row[2], row[3]]);
+                data.push([row[0],row[4], row[5], row[6]]);
+            });
+            model = data;
+
+
+            var creator = new ChartCreator();
+
+            creator.init({
+                model: model,
+                adapter: {
+
+                },
+                template: {},
+                creator: {},
+                onReady: renderCharts
+            });
+
+
+            function renderCharts(creator) {
+
+                var chartOne = creator.render({
+                    container: "#chartThree",
+                    creator: {
+                        chartObj: {
+                            chart: {
+                                type: "column"
+                            }
+                        }
+                    },
+                });
+            };
+        });
+
+
     });
 });
