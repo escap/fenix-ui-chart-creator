7c836cbae05aa3f9d9aa9dbed5a77ee219f392a2
simonefenix <simone.murzilli@fao.org>
2015-06-03 12:54:44 +0200

    fixed baseurl

--- src/js/paths.js
+++ src/js/paths.js
@@ -3,23 +3,23 @@ define(function () {
 
     'use strict';
 
-    var prefix = '../../src/js/';
-
     var config = {
+
+        baseUrl: '../../src/js/',
 
         paths : {
             //'fx-c-c': prefix,
-            'fx-c-c/start' : prefix + './start',
-            'fx-c-c/html' :  prefix + '../html',
-            'fx-c-c/config' :  prefix + '../../config',
-            'fx-c-c/adapters' :  prefix + './adapters',
-            'fx-c-c/templates' :  prefix + './templates',
-            'fx-c-c/creators' :  prefix + './creators',
+            'fx-c-c/start': './start',
+            'fx-c-c/html': '../html',
+            'fx-c-c/config': '../../config',
+            'fx-c-c/adapters':  './adapters',
+            'fx-c-c/templates': './templates',
+            'fx-c-c/creators': './creators',
 
             // third party libs
             text: '//fenixapps.fao.org/repository/js/requirejs/plugins/text/2.0.12/text',
             jquery: "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
-            highcharts : "//fenixapps.fao.org/repository/js/highcharts/4.0.4/js/highcharts", //'//code.highcharts.com/highcharts',
+            highcharts: "//fenixapps.fao.org/repository/js/highcharts/4.0.4/js/highcharts", //'//code.highcharts.com/highcharts',
             underscore: "//fenixapps.fao.org/repository/js/underscore/1.7.0/underscore.min",
             amplify: "//fenixapps.fao.org/repository/js/amplify/1.1.2/amplify.min",
             handlebars: "//fenixapps.fao.org/repository/js/handlebars/2.0.0/handlebars",
