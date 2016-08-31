41dcd9f7c69dc26f75b8a6aad6e39d36a525f50c
deneb1 <deneb1ster@gmail.com>
2016-08-31 16:55:38 +0200

    spider chart

--- src/config/renderers/highcharts.js
+++ src/config/renderers/highcharts.js
@@ -7,7 +7,22 @@ define(function () {
     'use strict';
 
     return {
-
+ spider: {
+            chart: {polar:true,type: 'line'},
+            title: {text: ''},
+            subtitle: {text: ''},
+            xAxis: {crosshair: true},
+            //yAxis: {min: 0, title: {text: ''}},
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} </b></td></tr>',
+                footerFormat: '</table>',
+                useHTML: true
+            },
+            plotOptions: {column: {pointPadding: 0.2, borderWidth: 0}},
+            series: []
+        },
         line: {
             chart: {type: 'line'},
             title: {text: ''},
@@ -300,7 +315,7 @@ define(function () {
             series: [{
                 name: '',
                 borderWidth: 1,
-                data: [/*[0, 0, 10], [0, 1, 19], [0, 2, 8], [0, 3, 24], [0, 4, 67], [1, 0, 92], [1, 1, 58], [1, 2, 78], [1, 3, 117], [1, 4, 48], [2, 0, 35], [2, 1, 15], [2, 2, 123], [2, 3, 64], [2, 4, 52], [3, 0, 72], [3, 1, 132], [3, 2, 114], [3, 3, 19], [3, 4, 16], [4, 0, 38], [4, 1, 5], [4, 2, 8], [4, 3, 117], [4, 4, 115], [5, 0, 88], [5, 1, 32], [5, 2, 12], [5, 3, 6], [5, 4, 120], [6, 0, 13], [6, 1, 44], [6, 2, 88], [6, 3, 98], [6, 4, 96], [7, 0, 31], [7, 1, 1], [7, 2, 82], [7, 3, 32], [7, 4, 30], [8, 0, 85], [8, 1, 97], [8, 2, 123], [8, 3, 64], [8, 4, 84], [9, 0, 47], [9, 1, 114], [9, 2, 31], [9, 3, 48], [9, 4, 91]*/],
+                data: [],
                 dataLabels: {
                     enabled: true,
                     color: '#000000'
@@ -345,7 +360,7 @@ define(function () {
 bubble:{
 
         chart: {
-            type: 'bubble',
+            type: 'bubble',polar:false,
             plotBorderWidth: 1,
             zoomType: 'xy'
         },
